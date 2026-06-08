398048d230203dcc6c4f03fe5c953f1873661b3f
Dane Madsen <dane_madsen@hotmail.com>
2026-06-08 11:21:13 +1000

    more work

--- app/src/main/java/app/grapheneos/speechservices/TtsSettingsActivity.kt
+++ app/src/main/java/app/grapheneos/speechservices/TtsSettingsActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.TextView
 import android.widget.Toolbar
 
@@ -13,14 +14,25 @@ class TtsSettingsActivity : Activity() {
         setContentView(R.layout.tts_settings_activity)
 
         val toolbar = findViewById<Toolbar>(R.id.action_bar)
-        toolbar.title = getString(R.string.app_name)
-        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_24)
+        setActionBar(toolbar)
+        actionBar?.apply {
+            title = getString(R.string.app_name)
+            setDisplayHomeAsUpEnabled(true)
+            setHomeActionContentDescription(R.string.settings_navigate_up)
+        }
         toolbar.navigationContentDescription = getString(R.string.settings_navigate_up)
-        toolbar.navigationIcon?.setTint(resolveColor(android.R.attr.textColorPrimary))
         toolbar.setNavigationOnClickListener { finishAfterTransition() }
 
         findViewById<TextView>(R.id.settings_version_value).text =
             versionName().ifEmpty { getString(R.string.settings_version_unknown) }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finishAfterTransition()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onNavigateUp(): Boolean {
@@ -35,13 +47,4 @@ private fun Context.versionName(): String {
         PackageManager.PackageInfoFlags.of(0),
     )
     return packageInfo.versionName.orEmpty()
-}
-
-private fun Context.resolveColor(attr: Int): Int {
-    val attrs = theme.obtainStyledAttributes(intArrayOf(attr))
-    return try {
-        attrs.getColor(0, 0)
-    } finally {
-        attrs.recycle()
-    }
 }
